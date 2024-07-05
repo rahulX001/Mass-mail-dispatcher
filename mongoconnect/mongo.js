b5a0b2954c35c383ee4f58a57863252905f8e83a
rahulX001 <rahul.kumarsonkar.eee21@itbhu.ac.in>
2024-07-05 20:01:39 +0530

    mongoose

--- mongoconnect/mongo.js
+++ mongoconnect/mongo.js
@@ -4,7 +4,9 @@ const url =process.env.MONGODB_URL;
 mongoose
   .connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   })
   .then(() => {
     console.log("connected to atlas");
